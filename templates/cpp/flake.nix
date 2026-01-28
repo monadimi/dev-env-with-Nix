f1fc17dbc32e999c224812b91dca9ce8911e47fb
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-29 08:22:22 +0900

    fixed: cpp flakes

--- templates/cpp/flake.nix
+++ templates/cpp/flake.nix
@@ -1,25 +1,75 @@
-{
-  description = "A Nix flake for vernye";
+{description = "Monad devShell: base";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          gcc gdb
-          meson ninja
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        rev = self.shortRev or self.rev or "dirty";
+
+        commonTools = with pkgs; [
+          git
+          curl
+          wget
+          unzip
+          zip
+          jq
+          gnused
+          gawk
+          which
+          tree
+          ripgrep
+          fd
         ];
 
-        shellHook = ''
-          exec ${pkgs.zsh}/bin/zsh
-        '';
-      };
-    }
-  );
+        fmtTools = with pkgs; [
+          nixfmt-rfc-style
+          deadnix
+          statix
+          shfmt
+          shellcheck
+          nodePackages.prettier
+        ];
+
+        extraTools = with pkgs; [
+          gcc
+          gdb
+          meson
+          ninja
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = commonTools ++ fmtTools ++ extraTools;
+          shellHook = ''
+            echo "Monad devShell (base) (${rev})"
+          '';
+
+        };
+
+        checks = {
+          nixfmt = pkgs.runCommand "check-nixfmt" { } ''
+            set -euo pipefail
+            find ${./.} -type f -name "*.nix" -print0 | xargs -0 ${pkgs.nixfmt-rfc-style}/bin/nixfmt --check
+            touch $out
+          '';
+
+          deadnix = pkgs.runCommand "check-deadnix" { } ''
+            set -euo pipefail
+            ${pkgs.deadnix}/bin/deadnix ${./.}
+            touch $out
+          '';
+
+          statix = pkgs.runCommand "check-statix" { } ''
+            set -euo pipefail
+            ${pkgs.statix}/bin/statix check ${./.}
+            touch $out
+          '';
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      });
 }
